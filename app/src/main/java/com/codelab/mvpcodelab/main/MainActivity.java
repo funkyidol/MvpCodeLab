d4ba56f23d8b3316ca49cf4d5dd63650d9c4fee6
Kshitij Aggarwal <funkyidol@gmail.com>
2016-10-08 08:23:15 +0530

    - Make the View-Presenter connections

--- app/src/main/java/com/codelab/mvpcodelab/main/MainActivity.java
+++ app/src/main/java/com/codelab/mvpcodelab/main/MainActivity.java
@@ -7,10 +7,14 @@ import com.codelab.mvpcodelab.R;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View{
 
+    MainContract.Presenter presenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        presenter = new MainPresenter(this);
     }
 
     @Override
--- app/src/main/java/com/codelab/mvpcodelab/main/MainPresenter.java
+++ app/src/main/java/com/codelab/mvpcodelab/main/MainPresenter.java
@@ -1,6 +1,13 @@
 package com.codelab.mvpcodelab.main;
 
 public class MainPresenter implements MainContract.Presenter {
+
+    private final MainContract.View view;
+
+    public MainPresenter(MainContract.View view) {
+        this.view = view;
+    }
+
     @Override
     public void onClickHipsterBtn() {
 
