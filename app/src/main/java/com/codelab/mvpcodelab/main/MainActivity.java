2dbfa20f60c2ef836bf9fcb407ec2bafb6aea696
Kshitij Aggarwal <funkyidol@gmail.com>
2016-10-08 08:26:34 +0530

    - Call to Presenter from View

--- app/src/main/java/com/codelab/mvpcodelab/main/MainActivity.java
+++ app/src/main/java/com/codelab/mvpcodelab/main/MainActivity.java
@@ -1,24 +1,41 @@
 package com.codelab.mvpcodelab.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.codelab.mvpcodelab.R;
 
-public class MainActivity extends AppCompatActivity implements MainContract.View{
+public class MainActivity extends AppCompatActivity implements MainContract.View {
 
     MainContract.Presenter presenter;
+
+    Button btnHipsterText;
+
+    TextView tvHipsterText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnHipsterText = (Button) findViewById(R.id.btn_hipster);
+        tvHipsterText = (TextView) findViewById(R.id.tv_text);
+
         presenter = new MainPresenter(this);
+
+        btnHipsterText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.onClickHipsterBtn();
+            }
+        });
     }
 
     @Override
     public void updateHipsterText(String hipsterText) {
-
+        tvHipsterText.setText(hipsterText);
     }
 }
