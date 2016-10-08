945af9d0ef6a4dce326980831b96a8f2fb1a7970
Kshitij Aggarwal <funkyidol@gmail.com>
2016-10-08 08:28:58 +0530

    - Presenter logic for handling view action and updation

--- app/src/main/java/com/codelab/mvpcodelab/main/MainPresenter.java
+++ app/src/main/java/com/codelab/mvpcodelab/main/MainPresenter.java
@@ -1,8 +1,14 @@
 package com.codelab.mvpcodelab.main;
 
+import com.codelab.mvpcodelab.model.HipsterRepo;
+import com.codelab.mvpcodelab.model.HipsterRepoImpl;
+import com.codelab.mvpcodelab.model.pojo.HipsterModel;
+
 public class MainPresenter implements MainContract.Presenter {
 
     private final MainContract.View view;
+
+    HipsterRepo hipsterRepo = new HipsterRepoImpl();
 
     public MainPresenter(MainContract.View view) {
         this.view = view;
@@ -10,6 +16,16 @@ public class MainPresenter implements MainContract.Presenter {
 
     @Override
     public void onClickHipsterBtn() {
+        hipsterRepo.getHipsterText(new HipsterRepo.GetHipsterTextCallback() {
+            @Override
+            public void onSuccess(HipsterModel hipsterModel) {
+                view.updateHipsterText(hipsterModel.getText());
+            }
 
+            @Override
+            public void onFailure(Throwable throwable) {
+                // TODO: 10/8/2016 Show toast
+            }
+        });
     }
 }
